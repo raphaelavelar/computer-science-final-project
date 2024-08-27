cf26e2e540876d7a13b6f07b8c37049afe22d2b5
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-08-27 23:43:02 +0000

    Add application user listing endpoint

--- backend/users/api.py
+++ backend/users/api.py
@@ -1,13 +1,23 @@
-from rest_framework.generics import CreateAPIView
+"""
+API for users application
+"""
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework import status
 from rest_framework.response import Response
-from users.serializers import ApplicationUserAuthenticationSerializer, UserAuthenticationSerializer
+from users.serializers import ApplicationUserAuthenticationSerializer, UserAuthenticationSerializer, ApplicationUserSerializer
+from users.models import ApplicationUser
 
-class ApplicationUserRegister(CreateAPIView):
+class ApplicationUserRegister(CreateAPIView, ListAPIView):
     """
     Register a new application user
     """
+    queryset = ApplicationUser.objects.all()
     serializer_class = UserAuthenticationSerializer
+
+    def get(self, request, *args, **kwargs):
+        self.serializer_class = ApplicationUserSerializer
+
+        return super().list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -19,3 +29,4 @@ class ApplicationUserRegister(CreateAPIView):
 
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
