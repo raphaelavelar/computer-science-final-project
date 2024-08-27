7b4b6a49f720b06809d4c36496a9ab9d5e816b46
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-08-27 13:16:44 +0000

    Add user register api

---
+++ backend/users/api.py
@@ -0,0 +1,19 @@
+from rest_framework.generics import CreateAPIView
+from users.serializers import ApplicationUserAuthenticationSerializer, UserAuthenticationSerializer
+
+class ApplicationUserRegister(CreateAPIView):
+    """
+    Register a new application user
+    """
+    serializer_class = UserAuthenticationSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        application_user_serializer = ApplicationUserAuthenticationSerializer(data={"user": serializer.validated_data})
+        application_user_serializer.is_valid(raise_exception=True)
+        application_user_serializer.save()
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
