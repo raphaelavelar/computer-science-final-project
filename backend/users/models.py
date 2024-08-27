ff7e72e177b89c300faebed5f5b653198ed878c0
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-08-27 12:32:16 +0000

    Add application user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,13 @@
+"""
+Models for users application
+"""
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ApplicationUser(models.Model):
+    """
+    Application user model
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=250, blank=True)
+    profile_picture = models.FileField(blank=True)
