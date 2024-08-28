ae802501550ea96a3fe9cb92c2b0ccdca6ac397e
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-08-28 23:35:49 +0000

    Add map to urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("users.urls")),
+    path("api/map/", include("map.urls")),
 ]
