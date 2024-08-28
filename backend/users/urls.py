ed6161db2f79b65b0b05902773a188cd04ea1cab
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-08-28 00:46:36 +0000

    Add user detail url

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,8 +3,9 @@ URLs for users application
 """
 
 from django.urls import path
-from users.api import ApplicationUserRegister
+from users.api import ApplicationUserRegister, ApplicationUserDetail
 
 urlpatterns = [
     path("", ApplicationUserRegister.as_view(), name="user_register"),
+    path("<int:pk>", ApplicationUserDetail.as_view(), name="user_detail"),
 ]
