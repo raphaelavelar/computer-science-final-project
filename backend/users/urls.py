e15d093418f1bc2376f1270f7f07436c0e039eb4
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-08-28 01:44:11 +0000

    Add login

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,9 +3,11 @@ URLs for users application
 """
 
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from users.api import ApplicationUserRegister, ApplicationUserDetail
 
 urlpatterns = [
     path("", ApplicationUserRegister.as_view(), name="user_register"),
     path("<int:pk>", ApplicationUserDetail.as_view(), name="user_detail"),
+    path("login/", obtain_auth_token, name="user_login"),
 ]
