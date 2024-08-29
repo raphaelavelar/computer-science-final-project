80cc715d73be0fbcf43d464b04c875792c3a01cd
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-08-29 00:02:45 +0000

    Create post model

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,3 +1,14 @@
+"""
+Models for posts application
+"""
 from django.db import models
+from users.models import ApplicationUser
 
-# Create your models here.
+class Post(models.Model):
+    """
+    Model for a post
+    """
+    author = models.OneToOneField(ApplicationUser, on_delete=models.DO_NOTHING)
+    publication_date = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=150, blank=False, null=False)
+    content = models.TextField(max_length=1500, blank=False, null=False)
