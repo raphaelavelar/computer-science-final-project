decdb22cfa1271ca86ddd3a1b5a699b7ef6b4501
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-08-29 01:01:39 +0000

    Add post creation serializer specification

--- backend/posts/api.py
+++ backend/posts/api.py
@@ -3,18 +3,23 @@ API for posts application
 """
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from posts.models import Post
-from posts.serializers import PostSerializer
+from posts.serializers import PostListSerializer, PostCreateSerializer, PostDetailSerializer
 
 class PostAll(ListCreateAPIView):
     """
     Create and list post
     """
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostCreateSerializer
+
+    def get(self, request, *args, **kwargs):
+        self.serializer_class = PostListSerializer
+
+        return self.list(request, *args, **kwargs)
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update and destroy a post
     """
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostDetailSerializer
