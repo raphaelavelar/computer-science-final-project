a56c2186414a6584c8a018d59a590be81e8d2333
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-08-29 01:01:44 +0000

    Update models

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -8,7 +8,8 @@ class Post(models.Model):
     """
     Model for a post
     """
-    author = models.OneToOneField(ApplicationUser, on_delete=models.DO_NOTHING)
-    publication_date = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(ApplicationUser, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=150, blank=False, null=False)
     content = models.TextField(max_length=1500, blank=False, null=False)
+    publication_date = models.DateTimeField(blank=False, null=False)
+    last_updated_at = models.DateTimeField(auto_now=True)
