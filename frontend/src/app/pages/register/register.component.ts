5820ff515632ce7e48889deedc1dff47119dcec8
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-08-30 02:14:58 +0000

    Add api request to register user

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user/user.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { UserRegister } from '../../interfaces/user';
 
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [],
+    imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
     templateUrl: './register.component.html',
     styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+    form: any;
 
+    constructor(
+        private _formBuilder: FormBuilder,
+        private _router: Router,
+        private _userService: UserService
+    ) {}
+
+    public ngOnInit() {
+        this.form = this._formBuilder.group({
+            username: ["", [Validators.required, Validators.maxLength(150)]],
+            email: ["", [Validators.required, Validators.email, Validators.maxLength(254)]],
+            password: ["", [Validators.required, Validators.maxLength(128)]],
+        })
+    }
+
+    public onSubmit() {
+        if(this.form.valid) {
+            const user: UserRegister = {
+                username: this.form.controls.username.value,
+                email: this.form.controls.email.value,
+                password: this.form.controls.password.value,
+            }
+            
+            this._userService.register(user).subscribe({
+                next: () => this._router.navigate(["/"]),
+                error: (error) => console.log(error)
+            })
+        }
+    }
 }
