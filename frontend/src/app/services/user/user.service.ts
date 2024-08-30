f039229eed62b235d9711e22ed029a6b52e920aa
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-08-30 02:15:28 +0000

    Add users register request

--- frontend/src/app/services/user/user.service.ts
+++ frontend/src/app/services/user/user.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { UserRegister } from '../../interfaces/user';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+    constructor(private _httpClient: HttpClient) { }
+
+    public register(registerUser: UserRegister): Observable<void> {
+        const url = `${environment.apiUrl}/users?format=api`;
+
+        return this._httpClient.post<void>(url, registerUser, {
+          headers: {
+            "Referrer-policy": "no-referrer",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Access-Control-Allow-Origin,Access-Control-Allow-Methods",
+            "Access-Control-Allow-Methods": "OPTION,GET,POST"
+          }
+        });
+    }
 }
