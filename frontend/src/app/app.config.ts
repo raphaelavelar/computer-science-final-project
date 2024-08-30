f8c1ee4b083ae19d427919d271b85f059bb10615
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-08-30 01:17:20 +0000

    Add angular material config provider

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -11,5 +12,6 @@ export const appConfig: ApplicationConfig = {
         provideRouter(routes),
         provideAnimationsAsync(),
         provideHttpClient(withFetch()),
+        {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline', floatLabel: "always"}}
     ]
 };
