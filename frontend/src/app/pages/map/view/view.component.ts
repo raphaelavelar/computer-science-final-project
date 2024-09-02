9acf2aca6dcb58d374f866a5506d91a5b46eb891
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-09-02 14:04:28 +0000

    Add map visualisation map

--- frontend/src/app/pages/map/view/view.component.ts
+++ frontend/src/app/pages/map/view/view.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { map, tileLayer } from 'leaflet';
 
+/*
+* This map visualisation was created following Leaflet's quick start guide
+* Reference: https://leafletjs.com/examples/quick-start/
+*/
 @Component({
   selector: 'app-view',
   standalone: true,
@@ -8,5 +13,11 @@ import { Component } from '@angular/core';
   styleUrl: './view.component.scss'
 })
 export class ViewComponent {
-
+  public ngOnInit() {
+    const mapView = map("map").setView([-29.625708, -52.747148], 7);
+    tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxNativeZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(mapView);
+  }
 }
