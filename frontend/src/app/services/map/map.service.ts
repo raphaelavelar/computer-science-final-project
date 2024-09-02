0bc97db49d799299ecb3dc7e560ef6b55cd5f646
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-09-02 15:26:30 +0000

    Create map service

--- frontend/src/app/services/map/map.service.ts
+++ frontend/src/app/services/map/map.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Map } from '../../interfaces/map';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class MapService {
 
-  constructor() { }
+    url = "";
+
+    constructor(private _httpClient: HttpClient) {
+        this.url = `${environment.apiUrl}/api/map/`;
+    }
+
+    public create(map: Map) {
+        return this._httpClient.post<Map>(this.url, map);
+    }
+
+    public list() {
+        return this._httpClient.get<Map[]>("aa");
+    }
 }
