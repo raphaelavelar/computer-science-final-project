1b27760f20163da18fe78fa7f4929e6e7fc2aabf
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-09-04 23:42:56 +0000

    Add guard to routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,26 +4,31 @@ import { LoginComponent } from './pages/login/login/login.component';
 import { ViewComponent } from './pages/map/view/view.component';
 import { CreateComponent as MapCreateComponent } from './pages/map/create/create.component';
 import { CreateComponent as PostCreateComponent } from './pages/posts/create/create.component';
+import { authGuard } from './guard/auth.guard';
 
 export const routes: Routes = [
     {
         path: "register",
-        component: RegisterComponent
+        component: RegisterComponent,
+        canActivate: [authGuard]
     },
     {
         path: "login",
-        component: LoginComponent
+        component: LoginComponent,
+        canActivate: [authGuard]
     },
     {
         path: "map/view",
-        component: ViewComponent
+        component: ViewComponent,
     },
     {
         path: "map",
-        component: MapCreateComponent
+        component: MapCreateComponent,
+        canActivate: [authGuard]
     },
     {
         path: "post",
-        component: PostCreateComponent
+        component: PostCreateComponent,
+        canActivate: [authGuard]
     },
 ];
