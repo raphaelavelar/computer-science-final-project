acdcb6c28e88e362b3293eecc053ef2c84faec8d
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-09-08 22:41:20 -0300

    Update map item model (#24)
    * Add requirements for drf-spectacular
    
    * Add drf spectacular to settings
    
    * Add api schema and swagger ui urls
    
    * Update map item model

--- backend/map/models.py
+++ backend/map/models.py
@@ -7,7 +7,7 @@ class MapItem(models.Model):
     """
     Model for a map item
     """
-    name = models.CharField(max_length=250)
-    description = models.TextField()
+    name = models.CharField(max_length=30)
+    description = models.TextField(max_length=500)
     latitude = models.FloatField()
     longitude = models.FloatField()
