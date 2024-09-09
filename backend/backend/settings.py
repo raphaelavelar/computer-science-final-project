f7a69239f0c17600eccd93af09899c567c4732bc
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2024-09-08 22:36:26 -0300

    Add API documentation (#23)
    * Add requirements for drf-spectacular
    
    * Add drf spectacular to settings
    
    * Add api schema and swagger ui urls

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
+    "drf_spectacular",
     "users.apps.UsersConfig",
     "map.apps.MapConfig",
     "posts.apps.PostsConfig",
@@ -94,8 +95,17 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-    ]
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Relevare",
+    "DESCRIPTION": "API documentation",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
+
 WSGI_APPLICATION = "backend.wsgi.application"
 
 
